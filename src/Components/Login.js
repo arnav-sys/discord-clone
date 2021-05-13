ec633ee41fb7ca41b87afded1fa8b916d23a737d
arnav-sys <programmeraggarwal@gmail.com>
2021-05-13 23:20:58 +0530

    first commit

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -12,9 +12,7 @@ const Login = () => {
     <div className="login">
       <div className="login__logo">
         <img src="https://discord.com/assets/fc0b01fe10a0b8c602fb0106d8189d9b.png" />
-        <h2>
-          Chat with your friends,co-workers all at the same time. Hello👋.
-        </h2>
+        <h2>discord clone made by arnav</h2>
       </div>
 
       <Button onClick={signUp}>Sign In</Button>
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import firebase from "firebase";
+/*import firebase from "firebase";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBb-7-c6IT5ZDGOwGTzSQ168IofmIgO1ts",
@@ -8,6 +8,27 @@ const firebaseConfig = {
   storageBucket: "react-authentication-tes-66381.appspot.com",
   messagingSenderId: "1079568641325",
   appId: "1:1079568641325:web:fb0177bb64fef7f78677a4",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
+*/
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA2qGav6kEMMMVy6T5vkRW4dWFaFT9m0Pg",
+  authDomain: "discord-clone-8545b.firebaseapp.com",
+  projectId: "discord-clone-8545b",
+  storageBucket: "discord-clone-8545b.appspot.com",
+  messagingSenderId: "1068798317102",
+  appId: "1:1068798317102:web:9f3283926359f5de5145bf",
+  measurementId: "G-XLQ3J5Z8RB",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
